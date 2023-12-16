6c71920edda89ae1e5b49ed170ff89807b850486
Imaginus02 <tom.reynaud@etu.emse.fr>
2023-12-16 16:01:30 +0100

    new activity action item

---
+++ ProjTechMobile/app/src/main/java/com/example/projtech/code_actions_item.kt
@@ -0,0 +1,11 @@
+package com.example.projtech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class code_actions_item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_code_actions_item)
+    }
+}
