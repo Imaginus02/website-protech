f5b87836072c0dfabf7332f7ea070d2ef9e4ee93
Imaginus02 <tom.reynaud@etu.emse.fr>
2024-01-08 14:03:42 +0100

    Added javadoc for SessionStatus enum

--- src/main/java/com/proj/tech/model/SessionStatus.java
+++ src/main/java/com/proj/tech/model/SessionStatus.java
@@ -1,3 +1,17 @@
 package com.proj.tech.model;
 
-public enum SessionStatus {ACTIVE, ARCHIVED}
+/**
+ * Enumeration representing the status of a session.
+ * The two possible values are ACTIVE and ARCHIVED.
+ */
+public enum SessionStatus {
+    /**
+     * Indicates that the session is active, meaning its end date has not yet passed.
+     */
+    ACTIVE,
+
+    /**
+     * Indicates that the session is archived, meaning its end date has passed.
+     */
+    ARCHIVED
+}
